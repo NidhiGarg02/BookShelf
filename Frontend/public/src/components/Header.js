224973d1dfa2e1e75f199120e6e9137cad857242
Mittal2002 <101242599+Mittal2002@users.noreply.github.com>
2023-11-23 04:33:13 +0000

    commit on various other components

---
+++ Frontend/public/src/components/Header.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import { NavLink } from "react-router-dom";
+const Header = () => {
+  const [value, setValue] = useState();
+  return (
+    <div>
+      <AppBar sx={{ backgroundColor: "Black" }} position="sticky">
+        <Toolbar>
+          <NavLink to="/" style={{ color: "white"  }}>
+            <Typography>
+              <LocalLibraryIcon></LocalLibraryIcon>
+            </Typography>
+          </NavLink>
+          <Tabs
+            sx={{ ml: "auto" }}
+            textColor="inherit"
+            indicatorColor="primary"
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            <Tab LinkComponent={NavLink} to="/add" label="Add product" />
+            <Tab LinkComponent={NavLink} to="/books" label="Books" />
+            <Tab LinkComponent={NavLink} to="/about" label="About Us" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
